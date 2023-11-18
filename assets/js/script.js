2748ccc3c46051f7b22022288914349ab2b1710a
Eric P <ericmpeterson04@gmail.com>
2023-11-18 16:10:10 -0700

    Added a working timer that appears when quiz start, counts down in seconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
+var timerElement = document.querySelector('.timer-count');
+
+var timerCount = 3;
+var timer;
+
 // At initialization, clear the main greeting and the start button
 var startQuiz = function () {
     document.querySelector('#main-greeting').style.display = "none";
     document.getElementById('start-button').style.display = "none";
+    startTimer();
+};
+
+// Setting the timer to decrease each second
+function startTimer() {
+    timer = setInterval(function() { 
+        timerCount --;
+        timerElement.textContent = "Time: "+timerCount;
+        
+        if (timerCount === 0) {
+            clearInterval(timer);
+        };
+    }, 1000);
 };
 
 var questionNumber = 0;
